b5cd298729feda281afb898f00a46ed8ddfffee3
blaz-cerpnjak <blaz003cerpnjak@gmail.com>
2022-02-05 16:49:34 +0100

    Add GameActivity

--- app/src/main/java/com/game/tictactoeapp/MainActivity.kt
+++ app/src/main/java/com/game/tictactoeapp/MainActivity.kt
@@ -2,10 +2,26 @@ package com.game.tictactoeapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        hideSystemBars()
+    }
+
+    /* we make our app fullscreen */
+    private fun hideSystemBars() {
+        val windowInsetsController =
+            ViewCompat.getWindowInsetsController(window.decorView) ?: return
+        // Configure the behavior of the hidden system bars
+        windowInsetsController.systemBarsBehavior =
+            WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+        // Hide both the status bar and the navigation bar
+        windowInsetsController.hide(WindowInsetsCompat.Type.systemBars())
     }
 }
